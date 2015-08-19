46efef86549836a11f390b1b7c15c73415234f70
Dennis Klein <d.klein@gsi.de>
2015-08-19 20:52:54 +0200

    add -t and -s flags

--- lib/miniasset/cli/datacenter.rb
+++ lib/miniasset/cli/datacenter.rb
@@ -4,15 +4,21 @@ require 'miniasset'
 module Miniasset::Cli
   class Datacenter < Thor
     desc 'list', 'List datacenters'
+    method_option 'table_header', aliases: '-t', desc: 'Control printing of the table header', type: :boolean, default: true
+    method_option 'simple', aliases: '-s', desc: 'Print a simple list of datacenter names, implies --no-table-header', type: :boolean, default: false
     def list
       app = Miniasset::Application.new
       header = '%2s %s'
       body = '%2d %s'
 
-      puts header % ['ID', 'NAME']
+      puts header % ['ID', 'NAME'] if !options['table_header'] || !options['simple']
 
       app.datacenters.each do |datacenter|
-        puts body % [datacenter.id, datacenter.name]
+        if options['simple']
+          puts datacenter.name
+        else
+          puts body % [datacenter.id, datacenter.name]
+        end
       end
     end
   end
